ececbeeefafd2c0b6941ed439a1a1372740f6fcc
esokoletsky <Esokoletsky@gmail.com>
2018-08-30 14:02:04 -0400

    changed tect in config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 "use strict"
  exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://esokoletsky:deskjet11@ds137102.mlab.com:37102/blogposts"; 
- exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-restaurants-app"; 
 exports.PORT = process.env.PORT || 8080;

cc271bb90b10007a4af5162ddb8f0c94c5764795
esokoletsky <Esokoletsky@gmail.com>
2018-08-30 13:35:12 -0400

    added config

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
 "use strict"
+ exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://esokoletsky:deskjet11@ds137102.mlab.com:37102/blogposts"; 
+ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-restaurants-app"; 
+exports.PORT = process.env.PORT || 8080;
